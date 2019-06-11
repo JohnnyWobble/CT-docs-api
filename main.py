e3c8d134919a3fe671ee8ca03191442eb8ce9270
JohnnyWobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-06-10 22:39:16 -0400

    test

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from setup import setup
-
-
+import setup
 
 
 def log(string):
@@ -15,61 +8,59 @@ def log(string):
 
 
 def even(number):
-
-  return any([number == i for i in [2, 4, 6, 8, 10, 12]])
-
+    return any([number == i for i in [2, 4, 6, 8, 10, 12]])
 
 
 def test():
-  testy = {'a': 23, 'b': 45}
-  print(testy)
-  testy.update({'a':45})
-  print(testy)
+    testy = {'a': 23, 'b': 45}
+    print(testy)
+    testy.update({'a': 45})
+    print(testy)
 
 # test()
 
 
-
-
 def find_str(test_str, str_to_find):
-  counter = 0
-  for i in test_str:
-    if i == str_to_find:
-      counter +=1
-  return counter
+    counter = 0
+    for i in test_str:
+        if i == str_to_find:
+            counter += 1
+    return counter
 
 
 def replace_string(string, find_string, replace_with):
-  return_string = ""
-  for i in string:
-    if i == find_string:
-      return_string += replace_with
-      continue
-    return_string += i
-  return return_string
+    return_string = ""
+    for i in string:
+        if i == find_string:
+            return_string += replace_with
+            continue
+        return_string += i
+    return return_string
 
 
 def main():
-    document = setup()
+    document = setup.setup()
     print('The title of the document is: {}'.format(document.get('title')))
     for i in document.get('body').get('content'):
-      # intel(i)
-      # print(i.get('paragraph').get('elements')[0].get('textRun').get('content'))
-      # input("[ENTER]")
-      try:
-        content = i.get('paragraph').get('elements')[0].get('textRun').get('content')
-        if content is not None: 
-          print(content)
-          if even(find_str(i.get('paragraph').get('elements')[0].get('textRun').get('content'), '`')):
-            print(replace_string(content, '`', '||||||'))
+        try:
+            content = i.get('paragraph').get('elements')[0].get('textRun').get('content')
+            if content is not None:
+                print(i)
+                print(content, end='')
+                if even(find_str(i.get('paragraph').get('elements')[0].get('textRun').get('content'), '`')):
+                    print(replace_string(content, '`', '||||||'), end='')
+                    print(i.get('startIndex'), i.get('endIndex'))
+                    final = setup.edit_request(startIndex=i.get('startIndex'), endIndex=i.get('endIndex'))
+                    response = setup.update(final)
+                    print(response)
 
-      except AttributeError as e:
-        print(f"no object: {e}")
-    # log(f"-  -  -\n{document.get('body')}")
+        except AttributeError as e:
+            print(f"no object: {e}")
+
 
 def intel(thing):
-  log(f"Size: {len(thing)}\nType: {type(thing)}\n---\n{thing}")
-  print(f"Size: {len(thing)}\nType: {type(thing)}\n---\n{thing}")
+    log(f"Size: {len(thing)}\nType: {type(thing)}\n---\n{thing}")
+    print(f"Size: {len(thing)}\nType: {type(thing)}\n---\n{thing}")
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -5,13 +5,63 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+
+sample_request = {
+  'updateTextStyle': {
+    'range': {
+      'segmentId': None,
+      'startIndex': None,
+      'endIndex': None
+    },
+    'textStyle': {
+      "bold": False,
+      "italic": False,
+      "underline": False,
+      "strikethrough": False,
+      "smallCaps": False,
+      "backgroundColor": {
+        'color': {
+          'rgbColor': {
+            'red': 0.2,
+            'green': 0.2,
+            'blue': 0.2
+          }
+        }
+      },
+      "foregroundColor": {
+        'color': {
+          'rgbColor': {
+            'red': 0.96,
+            'green': 0.96,
+            'blue': 0.96
+          }
+        }
+        },
+    "fontSize": {
+        'magnitude': 10,
+        'unit': 'PT'
+    },
+  "weightedFontFamily": {
+    'fontFamily': 'Courier New OS',
+    'weight': 400
+  },
+  "baselineOffset": 'NONE',
+  "link": None
+    }
+  }
+}
+
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/documents']
 
 # The ID of a sample document.
 DOCUMENT_ID = '1UeorM9adOh8Nds1Z457RRKBZMkh0VZ_kn_jllpkzh7U'
+service = None
+
 
 def setup():
+    global service
     """Shows basic usage of the Docs API.
     Prints the title of a sample document.
     """
@@ -39,3 +89,18 @@ def setup():
     # Retrieve the documents contents from the Docs service.
     document = service.documents().get(documentId=DOCUMENT_ID).execute()
     return document
+
+
+def edit_request(startIndex=None, endIndex=None):
+    sample_request.get('updateTextStyle').get('range').update({'startIndex': startIndex})
+    sample_request.get('updateTextStyle').get('range').update({'endIndex': endIndex})
+    wb = sample_request.get('updateTextStyle').get('range')
+    print(wb)
+    return sample_request
+
+
+def update(request):
+    result = service.documents().batchUpdate(
+        documentId=DOCUMENT_ID, body={'requests': [request]}).execute(num_retries=10)
+    return result
+
